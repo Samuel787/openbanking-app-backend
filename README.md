7f4ccb7e35de2b086af205df87d6e7cd47aa0ac2
Samuel787 <suther.david.samuel@gmail.com>
2022-05-10 18:05:18 +0800

    Initialized README

---
+++ README.md
@@ -0,0 +1,6 @@
+# Open Banking Deep Backend
+
+This project houses the deep backend for NUS FinTech Openbanking project. It consists of PostgreSQL db to store news and fx data and exposes some APIs for other components of the project to interact with the database. This project is developed using Python, Flask, PostgreSQL and Heroku.
+
+## Setting up the project locally
+
--- app.py
+++ app.py
@@ -19,8 +19,6 @@ app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # db = SQLAlchemy(app)
 db.init_app(app)
-# from models import Book
-# from models import Forex_Data
 
 @app.route("/")
 @cross_origin()
@@ -103,24 +101,6 @@ def get_fx_data():
             return deleteFXData(date)
         except Exception as e:
             return str(e)
-
-@app.route("/getall")
-@cross_origin()
-def get_all():
-    try:
-        books=Book.query.all()
-        return  jsonify([e.serialize() for e in books])
-    except Exception as e:
-	    return(str(e))
-
-@app.route("/get/<id_>")
-@cross_origin()
-def get_by_id(id_):
-    try:
-        book=Book.query.filter_by(id=id_).first()
-        return jsonify(book.serialize())
-    except Exception as e:
-	    return(str(e))
 
 if __name__ == '__main__':
     app.run()
